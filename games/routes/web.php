53dd30c1ce0d509c04f5f7b9497373bcad5248c3
Mohamed Boulahrouz <moboulah2008@gmail.com>
2026-09-24 09:40:03 +0200

    controllers,index,web

--- games/routes/web.php
+++ games/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+Route::get('games', [App\Http\Controllers\GameController::class, 'index']);
 
 Route::get('/', function () {
     return view('welcome');
